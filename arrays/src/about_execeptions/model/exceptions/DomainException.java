97325bef2ab42d5760e0f4b7178f09b42233f758
MatheusDario <dario.matheus97@gmail.com>
2022-04-14 12:21:00 -0300

    Good solution

---
+++ arrays/src/about_execeptions/model/exceptions/DomainException.java
@@ -0,0 +1,10 @@
+package about_execeptions.model.exceptions;
+
+public class DomainException extends Exception{
+    private static final long serealVersionUID =  1L;
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+
+}
