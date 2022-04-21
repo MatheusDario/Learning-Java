2fb233f27f06fcfcd6c620acc5f076b1e097562f
MatheusDario <dario.matheus97@gmail.com>
2022-04-21 13:12:39 -0300

    utilizando strams manualmente

--- course_java/src/about_files/application/Program.java
+++ course_java/src/about_files/application/Program.java
@@ -1,23 +1,38 @@
 package about_files.application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        File file = new File("/home/matheus/Documents/Files-course-java-udemy/file.txt");
-        Scanner sc = null;
+        String path = "/home/matheus/Documents/Files-course-java-udemy/file.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
-            }
+           fr = new FileReader(path);
+           br = new BufferedReader(fr);
+
+           String line = br.readLine();
+
+           while (line != null) {
+               System.out.println(line);
+               line = br.readLine();
+           }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         } finally {
-            if(sc != null) {
-               sc.close();
+            try {
+                if(fr != null) {
+                    fr.close();
+                }
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
