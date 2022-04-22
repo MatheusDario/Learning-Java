10c70997d9bcf0e8112bf8e6c0f9cd8638ac7d90
MatheusDario <dario.matheus97@gmail.com>
2022-04-22 11:53:50 -0300

    Study about Interfaces, first exam part 1.

---
+++ course_java/src/about_interfaces/application/Program.java
@@ -0,0 +1,7 @@
+package about_interfaces.application;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ course_java/src/about_interfaces/model/service/RentalService.java
@@ -0,0 +1,20 @@
+package about_interfaces.model.service;
+
+import about_interfaces.model.entities.CarRental;
+
+public class RentalService {
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+    }
+}
