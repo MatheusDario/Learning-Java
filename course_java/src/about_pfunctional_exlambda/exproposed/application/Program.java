31a437f4f71afea371cf8570569cc4d832e3542e
MatheusDario <dario.matheus97@gmail.com>
2022-04-28 13:35:19 -0300

    Update resolved last class about streams

---
+++ course_java/src/about_pfunctional_exlambda/exproposed/application/Program.java
@@ -0,0 +1,53 @@
+package about_pfunctional_exlambda.exproposed.application;
+
+import about_pfunctional_exlambda.exproposed.entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter full file path: ");
+        String path = sc.nextLine();
+        System.out.print("Enter salary: ");
+        Double salary = sc.nextDouble();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            List<Employee> employees = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                employees.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+            System.out.println("Email of people whose salary is more than " + salary + ":");
+
+            List<String> emails = employees.stream()
+                    .filter(emp -> emp.getSalary() > salary)
+                    .map(emp -> emp.getEmail())
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            emails.forEach(System.out::println);
+
+            Double sum = employees.stream()
+                            .filter(emp -> emp.getName().charAt(0) == 'A')
+                                    .map(emp -> emp.getSalary())
+                                            .reduce(0.0, (x,y) -> x +y );
+
+            System.out.println("Sum of salary of people whose name starts with 'A': " + sum);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        sc.close();
+    }
+}
